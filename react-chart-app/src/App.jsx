fbe9bc74f4fde2f97f61ef74c4f7e6d1c7deddf8
Laura Comar <lautmcomar@gmail.com>
2024-12-04 17:51:34 -0500

    app and initial

---
+++ react-chart-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BubbleChart from 'src/components/BubbleChart.jsx';
+
+function App() {
+  return (
+    <div>
+      <h1>My Simple Chart</h1>
+      <Bubble Chart />
+    </div>
+  );
+}
+
+export default App;
